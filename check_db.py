5eed3f2365a773265da00d05843231249cee7905
leoBitto <leonardobitto@gmail.com>
2025-08-21 21:24:46 +0200

    added check script

---
+++ check_db.py
@@ -0,0 +1,33 @@
+# init_db.py
+import json
+from google.cloud import secretmanager
+import psycopg2
+
+# --- 1. Leggi il secret DB dal Secret Manager ---
+SECRET_DB_NAME = "projects/trading-469418/secrets/db_info/versions/latest"
+client_sm = secretmanager.SecretManagerServiceClient()
+response = client_sm.access_secret_version(name=SECRET_DB_NAME)
+db_info = json.loads(response.payload.data.decode("UTF-8"))
+
+# --- 2. Connessione a PostgreSQL ---
+conn = psycopg2.connect(
+    host=db_info["DB_HOST"],
+    port=db_info["DB_PORT"],
+    database=db_info["DB_NAME"],
+    user=db_info["DB_USER"],
+    password=db_info["DB_PASSWORD"]
+)
+cursor = conn.cursor()
+
+# --- 3. Creazione tabella universe ---
+cursor.execute("""
+select *
+from universe
+);
+""")
+
+conn.commit()
+cursor.close()
+conn.close()
+
+print("Tabella 'universe' pronta!")

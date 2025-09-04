60a5e36a9e5be77bde6d1de6b704da55f7cdf1fc
leoBitto <leonardobitto@gmail.com>
2025-09-04 21:08:43 +0200

    eliminated backtest... for now

--- scripts/risk_manager.py
+++ scripts/risk_manager.py
@@ -145,7 +145,7 @@ def _calculate_atr(portfolio, ticker, period: int = 14) -> float:
     start_date = portfolio.date
     end_date = (datetime.strptime(start_date, '%Y-%m-%d') - timedelta(days=period)).strftime('%Y-%m-%d')
         
-    df = database.get_universe_data(start_date=start_date, end_date=end_date, ticker)
+    df = database.get_universe_data(start_date=start_date, end_date=end_date, ticker=ticker)
     if len(df) < 2:
         return 0.0
     
